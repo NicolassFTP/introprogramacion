66a14a3cd8518e165647904c15f2d352559a1b7a
NicolassFTP <diegonicos0462@gmail.com>
2022-10-21 17:17:40 -0500

    Creacion de nueva clase llamada virtualizacion

---
+++ src/Virtualizacion.java
@@ -0,0 +1,9 @@
+public class Virtualizacion {
+    public static void main(String[] args) {
+        System.out.print("TEMAS DE PROYECTOS EXPLICADOS EN CENTRO DE CONVENCIONES");
+        System.out.print("Creacion de entornos virtuales para el aprendizaje");
+        System.out.print("Entornos en Realidad virtual");
+        System.out.print("Desarrollo de videojuegos");
+
+    }
+}
